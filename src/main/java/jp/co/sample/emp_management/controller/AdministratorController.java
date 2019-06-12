272ec78a92664b5dbac7048591c5d5a0d9a420a3
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-12 17:05:37 +0900

    (2-1)中級　「〇〇さんこんにちは」と出るように修正。

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -125,6 +124,7 @@ public class AdministratorController {
 			model.addAttribute("loginError", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		session.setAttribute("administratorName", administrator.getName());
 		return "redirect:employee/showList";
 	}
 	
