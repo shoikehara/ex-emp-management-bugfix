95ff7135b541000487331cbcc09917cfa5c38c37
sho.ikehara <sho.ikehara@rakus.co.jp>
2019-06-13 12:15:37 +0900

    (6-2)エラーページの際の画面遷移(Whitelabel Error Page)が出ないように改修。

---
+++ src/main/java/jp/co/sample/emp_management/controller/NotFoundController.java
@@ -0,0 +1,17 @@
+package jp.co.sample.emp_management.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NotFoundController implements ErrorController {
+
+    private static final String PATH = "/error";
+    
+    @Override
+    @RequestMapping(PATH)
+    public String getErrorPath() {
+        return "notFound";
+    }
+}
